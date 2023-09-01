8ecc484fdb36101863f3d1ccbaf25a0b39202e32
PAX-12 <joaquin.suarezvallejos@gmail.com>
2023-08-31 22:33:08 -0300

    IntroductionMenu started

---
+++ Assets/C# Scripts/Periodic Table/StarterHydrogen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Atom
+{
+    public class StarterHydrogen : MonoBehaviour
+    {
+        [SerializeField] private Atom atom;
+        [SerializeField] private GameObject Hydrogen;
+        private TextMeshProUGUI[] text;
+        public Element Element { get; private set; }
+
+        private void Start()
+        {
+            Hydrogen.SetActive(true);
+        }
+
+        public void HideStarterHydrogen()
+        {
+            Hydrogen.SetActive(false);
+        }
+
+        private void Awake()
+        {
+            text = GetComponentsInChildren<TextMeshProUGUI>();
+
+            int protonCount = 1;
+
+            Element element = Elements.GetElement(protonCount);
+            if (element != null)
+            {
+                //Hook up button to show the element data
+                Button b = text[0].GetComponentInParent<Button>();
+
+                if (b != null)
+                {
+                    b.onClick.AddListener(() => SetElement(protonCount));
+                }
+            }
+        }
+
+        private void SetElement(int protonCount)
+        {
+            Debug.Log("Show element: " + Elements.GetElement(protonCount).Name);
+
+            if(atom != null)
+            {
+                atom.ForceToCommon(protonCount);
+            }
+        }
+    }
+}
