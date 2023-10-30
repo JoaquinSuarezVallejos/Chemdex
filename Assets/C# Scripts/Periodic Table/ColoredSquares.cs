5e051042011bede4cac86eff1958a54df04789c2
PAX-12 <joaquin.suarezvallejos@gmail.com>
2023-10-30 12:09:32 -0300

    coloredSquares update v2

--- Assets/C# Scripts/Periodic Table/ColoredSquares.cs
+++ Assets/C# Scripts/Periodic Table/ColoredSquares.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColoredSquares : MonoBehaviour
 {
-    public void RedShapeEnter()
-    {
-        Debug.Log("RedShape enter");
+    [SerializeField] GameObject[] Alkali_metals, Alkaline_earth_metals, Transition_metals, 
+        Post_Transition_metals, Metalloids, Nonmetals, 
+        Halogens, Noble_gases, Lanthanides, Actinides;
 
+    [SerializeField] GameObject[] allElements;
+
+    private Image image;
+
+    private void Update()
+    {
+        Alkali_metals = GameObject.FindGameObjectsWithTag("Alkali_metals");
+        Alkaline_earth_metals = GameObject.FindGameObjectsWithTag("Alkaline_earth_metals");
+        Transition_metals = GameObject.FindGameObjectsWithTag("Transition_metals");
+        Post_Transition_metals = GameObject.FindGameObjectsWithTag("Post_Transition_metals");
+        Metalloids = GameObject.FindGameObjectsWithTag("Metalloids");
+        Nonmetals = GameObject.FindGameObjectsWithTag("Nonmetals");
+        Halogens = GameObject.FindGameObjectsWithTag("Halogens");
+        Noble_gases = GameObject.FindGameObjectsWithTag("Noble_gases");
+        Lanthanides = GameObject.FindGameObjectsWithTag("Lanthanides");
+        Actinides = GameObject.FindGameObjectsWithTag("Actinides");
     }
 
-    public void RedShapeExit()
+    private void OnMouseOver()
     {
-        Debug.Log("RedShape exit");
+        switch (gameObject.name)
+        {
+            case "RedShapeTable": //Alkali_metals
+                foreach (GameObject g in Alkali_metals)
+                {
+                    image = g.GetComponent<Image>();
+                    Color tempColor = image.color;
+                    tempColor.a = 0.3f;
+                    image.color = tempColor;
+                }
+                break;
 
+            case "OrangeShapeTable":
+                Debug.Log("OrangeShape");
+                break;
+
+        }
+        
     }
+
 }
