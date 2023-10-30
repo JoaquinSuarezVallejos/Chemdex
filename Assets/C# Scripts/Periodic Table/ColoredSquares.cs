b508bccd580f1b0e8ae663d3287b13588e78f9dd
PAX-12 <joaquin.suarezvallejos@gmail.com>
2023-10-30 09:13:31 -0300

    coloredSquares update

---
+++ Assets/C# Scripts/Periodic Table/ColoredSquares.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredSquares : MonoBehaviour
+{
+    public void RedShapeEnter()
+    {
+        Debug.Log("RedShape enter");
+
+    }
+
+    public void RedShapeExit()
+    {
+        Debug.Log("RedShape exit");
+
+    }
+}
