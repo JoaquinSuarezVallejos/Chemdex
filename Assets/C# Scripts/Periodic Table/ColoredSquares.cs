1a75e3bbd70811a42b1941b7e353fcc7a56fede8
PAX-12 <joaquin.suarezvallejos@gmail.com>
2023-10-30 19:42:19 -0300

    coloredSquares update v3

--- Assets/C# Scripts/Periodic Table/ColoredSquares.cs
+++ Assets/C# Scripts/Periodic Table/ColoredSquares.cs
@@ -9,11 +9,12 @@ public class ColoredSquares : MonoBehaviour
         Post_Transition_metals, Metalloids, Nonmetals, 
         Halogens, Noble_gases, Lanthanides, Actinides;
 
-    [SerializeField] GameObject[] allElements;
+    [SerializeField] GameObject periodicTable;
 
     private Image image;
+    private Image[] allImagesOfElements;
 
-    private void Update()
+    private void Start()
     {
         Alkali_metals = GameObject.FindGameObjectsWithTag("Alkali_metals");
         Alkaline_earth_metals = GameObject.FindGameObjectsWithTag("Alkaline_earth_metals");
@@ -25,6 +26,8 @@ public class ColoredSquares : MonoBehaviour
         Noble_gases = GameObject.FindGameObjectsWithTag("Noble_gases");
         Lanthanides = GameObject.FindGameObjectsWithTag("Lanthanides");
         Actinides = GameObject.FindGameObjectsWithTag("Actinides");
+
+        allImagesOfElements = periodicTable.GetComponentsInChildren<Image>();
     }
 
     private void OnMouseOver()
@@ -32,21 +35,92 @@ public class ColoredSquares : MonoBehaviour
         switch (gameObject.name)
         {
             case "RedShapeTable": //Alkali_metals
-                foreach (GameObject g in Alkali_metals)
-                {
-                    image = g.GetComponent<Image>();
-                    Color tempColor = image.color;
-                    tempColor.a = 0.3f;
-                    image.color = tempColor;
-                }
-                break;
+                HideAllImages();
+                ShowAllImagesOfThisElement(Alkali_metals);
+            break;
 
-            case "OrangeShapeTable":
-                Debug.Log("OrangeShape");
-                break;
+            case "OrangeShapeTable": // Alkaline_earth_metals
+                HideAllImages();
+                ShowAllImagesOfThisElement(Alkaline_earth_metals);
+            break;
 
+            case "YellowShapeTable": // Transition_metals
+                HideAllImages();
+                ShowAllImagesOfThisElement(Transition_metals);
+            break;
+
+            case "GreenShapeTable": // Post_Transition_metals
+                HideAllImages();
+                ShowAllImagesOfThisElement(Post_Transition_metals);
+            break;
+
+            case "CyanShapeTable": // Metalloids
+                HideAllImages();
+                ShowAllImagesOfThisElement(Metalloids);
+            break;
+
+            case "BlueShapeTable": // Nonmetals
+                HideAllImages();
+                ShowAllImagesOfThisElement(Nonmetals);
+            break;
+
+            case "LightPurpleShapeTable": // Halogens
+                HideAllImages();
+                ShowAllImagesOfThisElement(Halogens);
+            break;
+
+            case "PurpleShapeTable": // Noble_gases
+                HideAllImages();
+                ShowAllImagesOfThisElement(Noble_gases);
+            break;
+
+            case "GrayShapeTable": // Lanthanides
+                HideAllImages();
+                ShowAllImagesOfThisElement(Lanthanides);
+            break;
+
+            case "TulipanVioletShapeTable": // Actinides
+                HideAllImages();
+                ShowAllImagesOfThisElement(Actinides);
+            break;
         }
-        
     }
 
+    private void OnMouseExit()
+    {
+        ShowAllImages();
+    }
+
+    private void ShowAllImages()
+    {
+        for (int i = 0; i < allImagesOfElements.Length; i++)
+        {
+            image = allImagesOfElements[i].GetComponent<Image>();
+            Color tempColor = image.color;
+            tempColor.a = 1f;
+            image.color = tempColor;
+        }
+    }
+
+    private void HideAllImages()
+    {
+        for (int i = 0; i < allImagesOfElements.Length; i++)
+        {
+            image = allImagesOfElements[i].GetComponent<Image>();
+            Color tempColor = image.color;
+            tempColor.a = 0.3f;
+            image.color = tempColor;
+        }
+    }
+
+    private void ShowAllImagesOfThisElement(GameObject[] ElementList)
+    {
+        foreach (GameObject element in ElementList)
+        {
+            image = element.GetComponent<Image>();
+            Color tempColor = image.color;
+            tempColor.a = 1f;
+            image.color = tempColor;
+        }
+    }
 }
